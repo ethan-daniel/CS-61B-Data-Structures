bd18227ff3c2f7a73d2d4c366cf59cb6942a9129
ethan-daniel <ethandaniel.pavlov@gmail.com>
2022-02-04 19:22:58 -0500

    TODO: update method

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,6 +1,7 @@
 import java.lang.Math;
 
 public class Planet {
+	static final double G = 6.67e-11;
     double xxPos, yyPos, xxVel, yyVel, mass;
     String imgFileName;
 
@@ -23,8 +24,53 @@ public class Planet {
 
     }
 
-    public double calcDistance(Planet p) {
-        return (Math.sqrt((p.xxPos - xxPos)*(p.xxPos - xxPos) + (p.yyPos - yyPos)*(p.yyPos - yyPos)));
+    public double calcDistance(Planet p){
+        return (Math.sqrt((p.xxPos - xxPos) * (p.xxPos - xxPos) + (p.yyPos - yyPos) * (p.yyPos - yyPos)));
     }
 
+	public double calcForceExertedBy(Planet p){
+		return (G * mass * p.mass)/(calcDistance(p) * calcDistance(p));
+	}
+	
+	public double calcForceExertedByX(Planet p){
+		return calcForceExertedBy(p) * (p.xxPos - xxPos)/calcDistance(p);
+	}
+	
+	public double calcForceExertedByY(Planet p){
+		return calcForceExertedBy(p) * (p.yyPos - yyPos)/calcDistance(p);
+	}
+	
+	public double calcNetForceExertedByX(Planet[] allPlanets){
+		double net = 0;
+		for (int i = 0; i != allPlanets.length; ++i){
+			if (!allPlanets[i].equals(this)){
+				net += calcForceExertedByX(allPlanets[i]);
+			}
+		}
+		return net;
+	}
+	
+	public double calcNetForceExertedByY(Planet[] allPlanets){
+		double net = 0;
+		for (int i = 0; i != allPlanets.length; ++i){
+			if (!allPlanets[i].equals(this)){
+				net += calcForceExertedByY(allPlanets[i]);
+			}
+		}
+		return net;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
