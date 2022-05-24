da8b343b863d7288d2887306bb9aa3a046949622
ethan-daniel <ethandaniel.pavlov@gmail.com>
2022-05-24 15:49:48 -0400

    Added methods to Solver and Board

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,54 @@
 package hw4.puzzle;
 
-public class Board {
+public class Board implements WorldState {
+
+    /** Constructs a board from an N-by-N array of tiles where
+     tiles[i][j] = tile at row i, column j */
+    public Board(int[][] tiles) {
+
+    }
+
+    /** Returns value of tile at row i, column j (or 0 if blank) */
+    public int tileAt(int i, int j) {
+        return 0;
+    }
+
+    /** Returns the board size N */
+    public int size() {
+        return 0;
+    }
+
+    /** Returns the neighbors of the current board */
+    public Iterable<WorldState> neighbors() {
+        return null;
+    }
+
+    /** The number of tiles in the wrong position. */
+    public int hamming() {
+        return 0;
+    }
+
+    /** The sum of the Manhattan distances
+     * (sum of the vertical and horizontal distance)
+     * from the tiles to their goal positions.*/
+    public int manhattan() {
+        return 0;
+    }
+
+    /** Estimated distance to goal. */
+    public int estimatedDistanceToGoal() {
+        return 0;
+    }
+
+    /** Returns true if this board's tile values are the same
+     position as y's */
+    public boolean equals(Object y) {
+        return false;
+    }
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +60,6 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,25 @@
+package hw4.puzzle;
+
+public class Solver {
+
+    /** Constructor which solves the puzzle, computing
+     everything necessary for moves() and solution() to
+     not have to solve the problem again. Solves the
+     puzzle using the A* algorithm. Assumes a solution exists. */
+    public Solver(WorldState inital) {
+
+    }
+
+    /** Returns the minimum number of moves to solve the puzzle starting
+     at the initial WorldState. */
+    public int moves() {
+
+        return 0;
+    }
+
+    /** Returns a sequence of WorldStates from the initial WorldState
+     to the solution. */
+    Iterable<WorldState> solution() {
+        return null;
+    }
+}
