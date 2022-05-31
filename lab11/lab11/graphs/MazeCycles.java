f437428cb1d7ae45cf25779aaab0b4618be64792
ethan-daniel <ethandaniel.pavlov@gmail.com>
2022-05-31 19:14:55 -0400

    Optional DFS and Cycle Check attempt

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,42 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private boolean cycleFound = false;
 
     public MazeCycles(Maze m) {
         super(m);
+        s = 0;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfsCycleFinder(s);
     }
 
     // Helper methods go here
+    private void dfsCycleFinder(int v) {
+        marked[v] = true;
+        announce();
+
+        if (cycleFound) {
+            return;
+        }
+
+        for (int w : maze.adj(v)) {
+            if (marked[w]) {
+                cycleFound = true;
+            }
+            if (!marked[w]) {
+                //edgeTo[w] = v;
+                announce();
+                //distTo[w] = distTo[v] + 1;
+                dfsCycleFinder(w);
+                if (cycleFound) {
+                    return;
+                }
+            }
+        }
+    }
 }
 
