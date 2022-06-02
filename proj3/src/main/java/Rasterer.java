39d270d4cce2735967b17e36514400188afa2d8e
ethan-daniel <ethandaniel.pavlov@gmail.com>
2022-06-02 15:45:04 -0400

    Added calculateULTile() to find the upper-left starting tile for the query box

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,3 +1,4 @@
+import javax.management.MBeanRegistration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,6 +11,7 @@ import java.util.Map;
 public class Rasterer {
     public static final double ROOT_ULLAT = 37.892195547244356, ROOT_ULLON = -122.2998046875,
             ROOT_LRLAT = 37.82280243352756, ROOT_LRLON = -122.2119140625;
+    public static final int TILE_SIZE = 256;
     private static final int MAX_DEPTH = 7;
 
     private String[][] render_grid;
@@ -20,10 +22,14 @@ public class Rasterer {
     private int depth;
     private boolean query_success;
     private double[] lonDPP_depth;
+    Map<String, Double> params;
     //private double width;
 
     public Rasterer() {
-        // YOUR CODE HERE
+        params = new HashMap<>();
+        lonDPP_depth = new double[MAX_DEPTH + 1];
+        generateLonDPPValues();
+
     }
 
     /**
@@ -57,8 +63,10 @@ public class Rasterer {
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+//        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
+//                           + "your browser.");
+        this.params = params;
+
         return results;
     }
 
@@ -76,31 +84,93 @@ public class Rasterer {
 
     /** Calculates the LonDPP values at each depth.  */
     private void generateLonDPPValues() {
-        lonDPP_depth = new double[MAX_DEPTH + 1];
-        for (int i = 0; i != MAX_DEPTH; ++i) {
-            lonDPP_depth[i] = calculateLonDPP(ROOT_LRLON, ROOT_ULLON, 256) /
+        for (int i = 0; i != MAX_DEPTH + 1; ++i) {
+            lonDPP_depth[i] = calculateLonDPP(ROOT_LRLON, ROOT_ULLON, TILE_SIZE) /
                     Math.pow(2, i);
         }
     }
 
+    /** Returns true if a number is between two other numbers. */
+    private boolean checkFits(double beginning, double end, double point) {
+//        System.out.println("Beginning: " + beginning + " End: " +
+//                end + " Point: " + point);
 
-    /** Returns the upper-left corner tile for the query box. */
+        return point >= beginning && point <= end;
+    }
+
+
+
+    /** Returns the upper-left corner tile for the query box.
+     * ULLON -> LRLON (+ direction)
+     * ULLAT -> LRLAT (- direction) */
     private String calculateULTile() {
         String tile = "";
 
         // Get depth
+        double expectedLonDPP = calculateLonDPP(params.get("lrlon"),
+                params.get("ullon"), TILE_SIZE);
+
+        int depth = 0;
+        while (expectedLonDPP < lonDPP_depth[depth]) {
+            ++depth;
+        }
 
         // Get x
-
-
         // Get y
+        int numImages = (int) Math.pow(4, depth);
+        int numImagesPerSide = (int) Math.sqrt(numImages);
+        double image_width = Math.abs((ROOT_ULLON - ROOT_LRLON) / numImagesPerSide);
+        double image_height = Math.abs((ROOT_ULLAT - ROOT_LRLAT) / numImagesPerSide);
+        double image_left_longitude = ROOT_ULLON;
+        double image_upper_latitude = ROOT_ULLAT;
+        int x = 0;
+        int y = 0;
+
+        for (; x != numImagesPerSide; ++x) {
+            if (image_left_longitude > params.get("ullon")) {
+                --x;
+                break;
+            }
+            image_left_longitude += image_width;
+        }
+
+        for (; y != numImagesPerSide; ++y) {
+            if (image_upper_latitude < params.get("ullat")) {
+                --y;
+                break;
+            }
+                image_upper_latitude -= image_height;
+        }
+
+        tile = "d" + depth + "_x" + x + "_y" + y + ".png";
+        //System.out.println(tile);
 
         return tile;
     }
 
+    /** For testing ONLY*/
+    public void tempSolve() {
+        calculateULTile();
+
+    }
+
+
     public static void main(String[] args) {
         Rasterer rast = new Rasterer();
         rast.generateLonDPPValues();
+
+        Map<String, Double> query = new HashMap<>();
+        query.put("lrlon", -122.24053369025242);
+        query.put("ullon", -122.24163047377972);
+        query.put("w",892.0);
+        query.put("h", 875.0);
+        query.put("ullat", 37.87655856892288);
+        query.put("lrlat", 37.87548268822065);
+
+        rast.getMapRaster(query);
+
+        rast.tempSolve();
+
     }
 
 }
